0bb30c1df3c1a4be050e62adeca6c2b7976460a4
Bianca Aparecida Alves Araujo <biancaaparecida07@gmail.com>
2023-11-24 16:50:51 -0300

    Aula 1

--- aula1.py
+++ aula1.py
@@ -1,9 +1,30 @@
+# Execução - python nomeDoArquivo.py
 # Comentário - ctrl + ;
+
 print ("Olá Mundo!")
 
-# Declaração de variáveis
+# Declaração de variáveis e atribuição de valores com o sinal de igual (=). 
+a = 0 # Leia-se A recebe 0.
 soma = 2 + 3
+# Concatenação e explicação de Print como função e utilização de parâmetros.
 print (soma)
+print ("O resultado da operação de soma é: ", soma)
 
-# Execução - python nomeDoArquivo.py
-if soma >= 10
+subtracao = 5 - 2
+print (subtracao)
+print ("O resultado da operação de subtracao é: ", subtracao)
+
+multiplicacao = 9 * 9
+print(multiplicacao)
+
+divisao = 8 / 2
+print (divisao)
+
+# Estrutura de controle - Condicionais
+# Input - Entrada de dados; Int - Transforma o valor digitado em inteiro.
+idade = int(input("Digite sua idade: "))
+
+if idade >= 25:
+    print ("Tem mais de 1/4 de século")
+
+
